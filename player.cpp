991da77366994513deb0937a962252ef38f34f20
André Agenor <andreagenor@icloud.com>
2019-02-06 14:32:05 -0200

    Add player code

--- player.cpp
+++ player.cpp
@@ -1,6 +1,72 @@
 #include "player.h"
 
-Player::Player()
+Player::Player(const QString &name, const int score)
+    : m_name(name)
+    , m_score(score)
 {
 
+}
+
+QString Player::name() const
+{
+    return m_name;
+}
+
+void Player::setName(const QString &name)
+{
+    m_name = name;
+}
+
+int Player::score() const
+{
+    return m_score;
+}
+
+void Player::setScore(int score)
+{
+    m_score = score;
+}
+
+void Player::addScore()
+{
+    ++m_score;
+}
+
+void Player::subtractScore()
+{
+    if (m_score > 0) {
+        --m_score;
+    }
+}
+
+Player &Player::operator ++()
+{
+    addScore();
+
+    return *this;
+}
+
+Player Player::operator ++(int)
+{
+    Player out(*this);
+
+    out.addScore();
+
+    return out;
+}
+
+Player &Player::operator --()
+{
+    subtractScore();
+
+    return *this;
+}
+
+Player Player::operator --(int)
+{
+    Player out(*this);
+
+    out.subtractScore();
+
+    return out;
 }
